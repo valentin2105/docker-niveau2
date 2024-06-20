8f8c011f27adbbda04f2df6131fcfdf41d4e88e6
Valentin OUVRARD <valentin@ops.nc>
2024-06-21 10:40:52 +1100

    create route ping

--- flask/app.py
+++ flask/app.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 def hello():
     return '<h1>Hello Formation Docker niveau 2 </h1>'
 
+@app.route('/ping')
+def ping():
+    return 'pong'
+
 if __name__ == "__main__":
     app.run(debug=True)
 
